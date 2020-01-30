212672a18047957f5732a246d6785973da37f8e5
Pablo Garrido <pablogs9@gmail.com>
2020-01-30 13:17:09 +0100

    Working DMA tx

--- Src/olimex_e407_serial_transport.c
+++ Src/olimex_e407_serial_transport.c
@@ -6,53 +6,68 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define ARRAY_LEN(x)            (sizeof(x) / sizeof((x)[0]))
+#define UART_DMA_BUFFER_SIZE 2048
+
+static uint8_t dma_buffer[UART_DMA_BUFFER_SIZE];
+static size_t dma_head = 0, dma_tail = 0;
+
+// static uint8_t * crtp_buffer = (uint8_t *)0x10000000;
 
 bool uxr_init_serial_platform(struct uxrSerialPlatform* platform, int fd, uint8_t remote_addr, uint8_t local_addr)
 {
-    switch ( fd ){
-        case 1:
-          platform->uart = &huart1;
-          break;
-        case 3:
-          platform->uart = &huart3;
-          break;
-        case 6:
-          platform->uart = &huart6;
-          break;
-        default:
-          return false;
-    }
-    return true;
+  switch ( fd ){
+      case 1:
+        platform->uart = &huart1;
+        break;
+      case 3:
+        platform->uart = &huart3;
+        break;
+      case 6:
+        platform->uart = &huart6;
+        break;
+      default:
+        return false;
+  }
+  HAL_UART_Receive_DMA(platform->uart, dma_buffer, UART_DMA_BUFFER_SIZE);
+  return true;
 }
 
 bool uxr_close_serial_platform(struct uxrSerialPlatform* platform)
-{
-    return true;
+{   
+  HAL_UART_DMAStop(platform->uart);
+  return true;
 }
 
 size_t uxr_write_serial_data_platform(uxrSerialPlatform* platform, uint8_t* buf, size_t len, uint8_t* errcode)
 {
   
   HAL_StatusTypeDef ret;
-  ret = HAL_UART_Transmit(platform->uart, buf, len, 10);
-  return ret == HAL_OK;
-}
+  // ret = HAL_UART_Transmit(platform->uart, buf, len, 1000);
 
-void hexprint(uint8_t* buf, size_t len){
-  for (size_t i = 0; i < len; i++) { DEBUG_PRINT("%X ",buf[i]); }
-  DEBUG_PRINT("\n");  
+  ret = HAL_UART_Transmit_DMA(platform->uart, buf, len);
+  while (ret == HAL_OK && platform->uart->gState != HAL_UART_STATE_READY){
+    osDelay(1);
+  }
+
+  return (ret == HAL_OK) ? len : 0;
 }
 
 size_t uxr_read_serial_data_platform(uxrSerialPlatform* platform, uint8_t* buf, size_t len, int timeout, uint8_t* errcode)
 { 
-  HAL_UART_Receive_DMA(platform->uart, buf, len);
-
+  size_t wrote = 0;
   osDelay(timeout);
 
-  uint16_t receivedBytes = ((uint16_t) len) - __HAL_DMA_GET_COUNTER(platform->uart->hdmarx);
+  __disable_irq();
+  dma_tail = UART_DMA_BUFFER_SIZE - __HAL_DMA_GET_COUNTER(platform->uart->hdmarx);
+  __enable_irq();
 
-  HAL_UART_DMAStop(platform->uart);
+  if (((dma_head + len) % UART_DMA_BUFFER_SIZE) <= dma_tail){ // Buffer has more or eq data than required
+    wrote = len;
+  }else if (((dma_head + len) % UART_DMA_BUFFER_SIZE) > dma_tail){ // Buffer has less data than required
+    wrote = dma_tail - dma_head;
+  }
+  memcpy(buf, &dma_buffer[dma_head],wrote);
+  dma_head += wrote;
   
-  return receivedBytes;
+  return wrote;
 }
